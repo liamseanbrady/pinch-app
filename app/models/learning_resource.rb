6023af18a5211dcf7922e2c224cfa727fb4eb1de
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-27 23:57:18 +0000

    Add belongs_to for goal and submitter(user)

--- app/models/learning_resource.rb
+++ app/models/learning_resource.rb
@@ -1,4 +1,7 @@
 class LearningResource < ActiveRecord::Base
+  belongs_to :goal
+  belongs_to :submitter, foreign_key: 'user_id', class_name: 'User'
+
   validates :url, presence: true
   validates :summary, length: {minimum: 15}
 end
