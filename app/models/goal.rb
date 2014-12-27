58f61a0a3f9e0fa85804dcefe64d98e1b0b9b444
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-27 22:09:27 +0000

    Add belongs_to association for creator (User)

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,6 @@
 class Goal < ActiveRecord::Base
+  belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
+
   validates :title, length: {minimum: 6}
   validates :description, length: {minimum: 15, maximum: 100}
 end
