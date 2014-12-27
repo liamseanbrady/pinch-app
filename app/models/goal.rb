d45fafffaf9c903149f0aa8ed0a106b03a934d2f
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-27 23:35:05 +0000

    Add has_one association for category

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,6 @@
 class Goal < ActiveRecord::Base
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
+  has_one :category
 
   validates :title, length: {minimum: 6}
   validates :description, length: {minimum: 15, maximum: 100}
