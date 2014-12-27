f40091c904f7ef49165a06393576f9a803d5fbc4
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-27 23:22:26 +0000

    Add role column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141227215422) do
+ActiveRecord::Schema.define(version: 20141227232055) do
 
   create_table "goals", force: true do |t|
     t.string   "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20141227215422) do
     t.string   "tagline"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
 end
