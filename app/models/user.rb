b3fa8fa13475bb5acd42b160ada50a53cdf32df3
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-27 22:07:50 +0000

    Add has_many association for goals

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password validations: false
 
+  has_many :goals
+
   validates :password, length: {minimum: 8}, on: :create
   validates :username, length: {minimum: 3, maximum: 18}
   validates :email, presence: true, format: { with: /\A[\w\d]+[@][a-z]+.(com|co.uk)\z/ }, on: :create
