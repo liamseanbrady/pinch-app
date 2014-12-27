0d3b7219df5b81b8b5f7119817660a5ef7d9a781
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-27 23:30:26 +0000

    Add table for categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141227232055) do
+ActiveRecord::Schema.define(version: 20141227232415) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.integer  "goal_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "goals", force: true do |t|
     t.string   "title"
