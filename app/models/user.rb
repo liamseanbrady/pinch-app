00715ffc1b4959da577dec47b8e33740a8716758
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-27 21:44:13 +0000

    Add validations to password, username, email, and tagline

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
 
   validates :password, length: {minimum: 8}, on: :create
+  validates :username, length: {minimum: 3, maximum: 18}
+  validates :email, presence: true, format: { with: /\A[\w\d]+[@][a-z]+.(com|co.uk)\z/ }, on: :create
+  validates :tagline, length: {maximum: 35}
 end
