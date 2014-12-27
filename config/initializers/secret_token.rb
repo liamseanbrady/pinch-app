50c9a1ce5888074d4d903c30b0df212f9540cd79
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-27 21:10:32 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PinchApp::Application.config.secret_key_base = '5c3b04281185b437af7f2b9b976957baa05fb7667c9f2cd606bbd0b7d1df801209c9926589ad68a8f6d2998522725786d1826b997a6823995619647a3c0f1845'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PinchApp::Application.config.session_store :cookie_store, key: '_pinch-app_session'
