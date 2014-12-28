522db6a9fdcaef9ecae191af723285c2c0a50d8e
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-28 22:06:57 +0000

    Add new and create actions with logic for creating a new resource

--- app/controllers/learning_resources_controller.rb
+++ app/controllers/learning_resources_controller.rb
@@ -1,10 +1,25 @@
 class LearningResourcesController < ApplicationController
   def new
-    @goal = Goal.find(params[:id])
+    @goal = Goal.find(params[:goal_id])
     @learning_resource = LearningResource.new
   end
 
   def create
+    @learning_resource = LearningResource.new(learning_resource_params)
+    @goal = Goal.find(params[:goal_id])
+    @goal.learning_resources << @learning_resource
 
+    if @learning_resource.save
+      flash[:notice] = 'Your new resource was added to the goal'
+      redirect_to goals_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def learning_resource_params
+    params.require(:learning_resource).permit(:url, :summary)
   end
 end
