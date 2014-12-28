16fd0890aa4daff2bafa90061c2bbf6553b19412
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-28 20:14:30 +0000

    Allow all routes for goals except destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 PinchApp::Application.routes.draw do
   root to: 'goals#index'
 
-  resources :goals, only: [:index, :new, :create, :show]
+  resources :goals, except: [:destroy]
   resources :categories, only: [:new, :create]
 end
