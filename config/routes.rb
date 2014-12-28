63806673ac812b2d6314ac42fbd47e4cc567c236
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-28 21:26:13 +0000

    Add nested route in goals for pinch action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 PinchApp::Application.routes.draw do
   root to: 'goals#index'
 
-  resources :goals, except: [:destroy]
+  resources :goals, except: [:destroy] do
+    member do
+      post :pinch
+    end
+  end
   resources :categories, only: [:new, :create]
 end
