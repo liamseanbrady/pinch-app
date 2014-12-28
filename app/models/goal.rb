67d64f4556f85d6205f23d7a7d4add99f8e1229c
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-28 00:08:42 +0000

    Add has_many association for learning resources

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,6 +1,7 @@
 class Goal < ActiveRecord::Base
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_one :category
+  has_many :learning_resources
 
   validates :title, length: {minimum: 6}
   validates :description, length: {minimum: 15, maximum: 100}
