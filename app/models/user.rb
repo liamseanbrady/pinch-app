e9eb078aace5ac63539b24722bfd9cae98044591
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-28 01:00:36 +0000

    Add has_many_through association for goals which user is contributing to

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   has_many :goals
   has_many :learning_resources
 
+  has_many :contribution_permissions
+  has_many :contributing_goals, through: :contribution_permissions, source: :goal
+
   validates :password, length: {minimum: 8}, on: :create
   validates :username, length: {minimum: 3, maximum: 18}
   validates :email, presence: true, format: { with: /\A[\w\d]+[@][a-z]+.(com|co.uk)\z/ }, on: :create
