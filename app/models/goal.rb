d01036b964e235eb85830ff7e1456790c805a334
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-28 01:03:08 +0000

    Add has_many_through association for users which are contributing to a goal

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,6 +3,9 @@ class Goal < ActiveRecord::Base
   has_one :category
   has_many :learning_resources
 
+  has_many :contribution_permissions
+  has_many :contributors, through: :contribution_permissions, source: :user
+
   validates :title, length: {minimum: 6}
   validates :description, length: {minimum: 15, maximum: 100}
 end
