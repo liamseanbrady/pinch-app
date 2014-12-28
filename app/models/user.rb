974706b81ff6ebc0904b82aa45401e2511156e47
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-28 02:14:19 +0000

    Add has_many_through association for tracking goals a user has pinched

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ActiveRecord::Base
   has_many :contribution_permissions
   has_many :contributing_goals, through: :contribution_permissions, source: :goal
 
+  has_many :pinch_relationships
+  has_many :pinches, through: :pinch_relationships, source: :goal
+
   validates :password, length: {minimum: 8}, on: :create
   validates :username, length: {minimum: 3, maximum: 18}
   validates :email, presence: true, format: { with: /\A[\w\d]+[@][a-z]+.(com|co.uk)\z/ }, on: :create
