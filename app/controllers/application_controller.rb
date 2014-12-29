bc00cd9d34c5d31a05e8c595b441ec330f2a5c8f
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-29 14:59:55 +0000

    Add method to require admin role on user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,11 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
     end
   end
+
+  def require_admin
+    if !logged_in? || logged_in? && !current_user.admin?
+      flash[:error] = "You don't have permission to do that"
+      redirect_to root_path
+    end
+  end
 end
