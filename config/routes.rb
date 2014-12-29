2251bb66a533e5e607559b2bc5881c46ef98780d
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-29 01:55:55 +0000

    Add all routes for users apart from destroy. Alias users#new as register.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 PinchApp::Application.routes.draw do
   root to: 'goals#index'
 
+  get '/register', to: 'users#new', as: 'register'
+
+  resources :users, only: [:create, :edit, :update, :show]
   resources :goals, except: [:destroy] do
     resources :learning_resources, only: [:new, :create]
     member do
