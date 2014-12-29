b26a588808bb215a5e0d384c5e3e7267c73bde51
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-29 02:46:31 +0000

    Add logic for create and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,22 @@
 class SessionsController < ApplicationController
-  def new
-
-  end
+  def new; end
 
   def create
+    user = User.find_by(params[:username])
 
+    if user && user.authenticate(params[:password])
+      flash[:notice] = 'You logged in successfully'
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash.now[:error] = "There's something wrong with your username or password"
+      render :new
+    end
   end
 
   def destroy
-
+    flash[:notice] = 'You logged out successfully'
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
