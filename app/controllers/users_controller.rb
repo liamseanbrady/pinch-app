43cbaf068a38817c2d5e14b64542d8d9199f0710
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-29 02:26:43 +0000

    Add create action logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,14 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
 
+    if @user.save
+      flash[:notice] = 'You registered successfully'
+      redirect_to goals_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -17,5 +24,11 @@ class UsersController < ApplicationController
 
   def show
 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :tagline, :password)
   end
 end
