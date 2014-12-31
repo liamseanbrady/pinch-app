115f04479a405911c31f78864056a3ea9f70a5ce
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-31 11:42:24 +0000

    Add methods for marking accepted column as true or false

--- app/models/contribution_request.rb
+++ app/models/contribution_request.rb
@@ -2,4 +2,12 @@ class ContributionRequest < ActiveRecord::Base
   belongs_to :sender, foreign_key: 'sender_id', class_name: 'User'
   belongs_to :recipient, foreign_key: 'recipient_id', class_name: 'User'
   belongs_to :goal
+
+  def mark_as_accepted
+    self.accepted = true
+  end
+
+  def mark_as_rejected
+    self.accepted = false
+  end
 end
