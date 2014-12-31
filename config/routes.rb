0205a8f37a6bed072661dd5ec077285011bf5d54
Liam Sean Brady <liamseanbrady@gmail.com>
2014-12-31 12:54:09 +0000

    Include destroy route for contribution_requests resources

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ PinchApp::Application.routes.draw do
   end
   resources :goals, except: [:destroy] do
     resources :learning_resources, only: [:new, :create]
-    resources :contribution_requests, only: [:create, :update]
+    resources :contribution_requests, only: [:create, :update, :destroy]
     member do
       post :pinch
       post :drop
