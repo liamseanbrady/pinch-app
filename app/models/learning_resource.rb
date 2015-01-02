c7b49c55635d9215ee0874312b27d3765548bc03
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-02 23:32:57 +0000

    Add has_many association for ratings

--- app/models/learning_resource.rb
+++ app/models/learning_resource.rb
@@ -1,6 +1,7 @@
 class LearningResource < ActiveRecord::Base
   belongs_to :goal
   belongs_to :submitter, foreign_key: 'user_id', class_name: 'User'
+  has_many :ratings, as: :rateable
 
   validates :url, presence: true
   validates :summary, length: {minimum: 15}
