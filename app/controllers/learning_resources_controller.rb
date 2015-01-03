189eeaa654a535db5f313f0c94e4afa8ad787fb3
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-03 02:09:04 +0000

    Add logic for liking a resource. Add before actions for stopping the resource submitter from liking their own resource and allowing only pinchers to like a resource.

--- app/controllers/learning_resources_controller.rb
+++ app/controllers/learning_resources_controller.rb
@@ -1,7 +1,10 @@
 class LearningResourcesController < ApplicationController
-  before_action :set_goal, only: [:new, :create]
-  before_action :require_user, only: [:new, :create]
+  before_action :set_learning_resource, only: [:like]
+  before_action :set_goal, only: [:new, :create, :like]
+  before_action :require_user, only: [:new, :create, :like]
   before_action :require_goal_creator_or_contributor, only: [:new, :create]
+  before_action :disallow_resource_creator, only: [:like]
+  before_action :require_pincher, only: [:like]
 
   def new
     @learning_resource = LearningResource.new
@@ -20,10 +23,26 @@ class LearningResourcesController < ApplicationController
     end
   end
 
+  def like
+    like = Like.create(creator: current_user, likeable: @learning_resource)
+
+    if like.valid?
+      flash[:notice] = 'You liked a resource!'
+    else
+      flash[:error] = 'There was an error liking the resource. Try again.'
+    end
+
+    redirect_to :back
+  end
+
   private
 
   def learning_resource_params
     params.require(:learning_resource).permit(:url, :summary)
+  end
+
+  def set_learning_resource
+    @learning_resource = LearningResource.find(params[:id])
   end
 
   def set_goal
@@ -34,6 +53,20 @@ class LearningResourcesController < ApplicationController
     if current_user != @goal.creator && !@goal.contributor?(current_user)
       flash[:error] = "You don't have permission to do that"
       redirect_to root_path
+    end
+  end
+
+  def disallow_resource_creator
+    if current_user == @learning_resource.submitter
+      flash[:error] = "You can't like a resource you submitted"
+      redirect_to :back
+    end
+  end
+
+  def require_pincher
+    if !@goal.pincher?(current_user)
+      flash[:error] = 'You have to pinch a goal in order to like it!'
+      redirect_to :back
     end
   end
 end
