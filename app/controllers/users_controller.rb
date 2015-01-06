36b232f591528c52d79bdb47f2df3c465d219b97
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-06 00:50:11 +0000

    Fix omission of only: in set_user. Was causing bug where trying to set_user even when registering.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, [:edit, :update, :show, :dashboard]
+  before_action :set_user, only: [:edit, :update, :show, :dashboard]
   before_action :require_user, only: [:edit, :update, :show, :dashboard]
   before_action :require_same_user, only: [:edit, :update, :dashboard]
 
