240f90df559d25efce1e35b8bf836e7c8f0222ec
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-13 03:50:53 +0000

    Remove after_create callback to created a pinch notification and remove method to create pinch notification too.

--- app/models/pinch_relationship.rb
+++ app/models/pinch_relationship.rb
@@ -1,11 +1,4 @@
 class PinchRelationship < ActiveRecord::Base
   belongs_to :user
   belongs_to :goal
-
-  after_create :create_pinch_notification
-
-  # TODO: Move somewhere else - this seems dirty. Should this model really know about another that it doesn't directly interact with?
-  def create_pinch_notification
-    PinchNotification.create(pincher: self.user, goal: self.goal, goal_creator: self.goal.creator)
-  end
 end
