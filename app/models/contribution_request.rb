04f4f1c2591df0b5cdc90ff8eaabc5ce90bb644e
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-16 23:44:40 +0000

    Add dependent_goal_dropped method to delelete contribution request if the goal it belongs to is dropped by the sender.

--- app/models/contribution_request.rb
+++ app/models/contribution_request.rb
@@ -3,6 +3,10 @@ class ContributionRequest < ActiveRecord::Base
   belongs_to :recipient, foreign_key: 'recipient_id', class_name: 'User'
   belongs_to :goal
 
+  def dependent_goal_dropped
+    self.delete
+  end
+
   def mark_as_accepted
     self.update(accepted: true)
   end
