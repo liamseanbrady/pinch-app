f1205f711f2ee0914152b50caad21a90cd94d846
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-18 10:44:55 +0000

    Add scopes to allow cleaner access to specific groups of contribution requests.

--- app/models/contribution_request.rb
+++ app/models/contribution_request.rb
@@ -3,6 +3,11 @@ class ContributionRequest < ActiveRecord::Base
   belongs_to :recipient, foreign_key: 'recipient_id', class_name: 'User'
   belongs_to :goal
 
+  scope :pending, -> { where accepted: nil }
+  scope :accepted, -> { where.not accepted: nil}
+  scope :unread, -> { where viewed_at: nil }
+  scope :read, -> { where.not viewed_at: nil }
+
   def dependent_goal_dropped
     self.delete
   end
