bd01488b406ad743864e8d4b0e8fb409144cb897
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-18 10:46:44 +0000

    Add scopes to allow easier access to only read or unread pinch notifications.

--- app/models/pinch_notification.rb
+++ app/models/pinch_notification.rb
@@ -2,4 +2,7 @@ class PinchNotification < ActiveRecord::Base
   belongs_to :goal_creator, class_name: 'User'
   belongs_to :pincher, class_name: 'User'
   belongs_to :goal
+
+  scope :read, -> { where.not viewed_at: nil }
+  scope :unread, -> { where viewed_at: nil }
 end
