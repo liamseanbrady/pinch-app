1053aaa4b56892ab59fe87b8a02e43b295fe2ae5
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-28 03:41:14 +0000

    Add method to prepend http:// to urls if they don't have that already.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def display_datetime(dt)
     dt.strftime("%d %b %Y")
   end
+
+  def friendly_url(url)
+    url.start_with?('http://') ? url : "http://#{url}"
+  end
 end
