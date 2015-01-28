302fb39a2b861e93bb5051d5cb7dff0554426249
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-28 20:19:38 +0000

    Make sure user's email is downcased before using it to search for the user in the database.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(username: params[:username]) || User.find_by(email: params[:username])
+    user = User.find_by(username: params[:username]) || User.find_by(email: params[:username].downcase)
 
     if user && user.authenticate(params[:password])
       # flash[:notice] = 'You logged in successfully'
