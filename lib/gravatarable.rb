e3c3c4b75351c5fa937892c42be3bbb638e60fd7
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-29 07:09:41 +0000

    Change call_class_setup to class_setup.

--- lib/gravatarable.rb
+++ lib/gravatarable.rb
@@ -3,7 +3,7 @@ module Gravatarable
     base.send(:include, InstanceMethods)
     base.extend ClassMethods
     base.class_eval do
-      call_class_setup
+      class_setup
     end
   end
 
@@ -26,7 +26,7 @@ module Gravatarable
   end
 
   module ClassMethods
-    def call_class_setup
+    def class_setup
       class_attribute :email_addr
       before_save :gravatar_url if "#{self.email_addr}_changed?".to_sym
     end
