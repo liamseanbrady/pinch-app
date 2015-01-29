27d3ef17aec37a726504d4d5497549057e9cadec
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-29 23:09:45 +0000

    Redirect to all goals path if log in successful. Remove comments.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,8 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username]) || User.find_by(email: params[:username].downcase)
 
     if user && user.authenticate(params[:password])
-      # flash[:notice] = 'You logged in successfully'
       session[:user_id] = user.id
-      redirect_to root_path
+      redirect_to goals_path
     else
       flash.now[:error] = "There's something wrong with your username or password"
       render :new
@@ -15,7 +14,6 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    # flash[:notice] = 'You logged out successfully'
     session[:user_id] = nil
     redirect_to root_path
   end
